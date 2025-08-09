05b84d393312f1597541657f06670ef9a2c554fc
Aaron <aaron.stacey@proton.me>
2025-08-09 09:09:50 -0500

    Add file naming

--- mp3tc.py
+++ mp3tc.py
@@ -1,9 +1,18 @@
 from ffmpeg import FFmpeg
 
+input_name:str = "208-led_zeppelin-nobodys_fault_but_mine.flac"
+
+def clean_name(input_name: str) -> str:
+    output_name = input_name.split(".")[0]
+    output_name += ".mp3"
+    return output_name
+
+output_name:str = clean_name(input_name)
+
 ffmpeg = (
     FFmpeg()
-    .input("208-led_zeppelin-nobodys_fault_but_mine.flac")
-    .output("output.mp3", {"codec:a": "libmp3lame", "q:a": "0"}, preset="veryslow")
+    .input(input_name)
+    .output(output_name, {"codec:a": "libmp3lame", "q:a": "0"}, preset="veryslow")
 )
 
 ffmpeg.execute()
