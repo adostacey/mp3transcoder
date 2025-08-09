534dd612cd36cd87c0ee80f882a740d644f733f2
Aaron <aaron.stacey@proton.me>
2025-08-09 09:27:13 -0500

    Convert to cli and some error handling

--- mp3tc.py
+++ mp3tc.py
@@ -1,18 +1,30 @@
+import sys
 from ffmpeg import FFmpeg
 
-input_name:str = "208-led_zeppelin-nobodys_fault_but_mine.flac"
 
 def clean_name(input_name: str) -> str:
     output_name = input_name.split(".")[0]
     output_name += ".mp3"
     return output_name
 
-output_name:str = clean_name(input_name)
 
-ffmpeg = (
-    FFmpeg()
-    .input(input_name)
-    .output(output_name, {"codec:a": "libmp3lame", "q:a": "0"}, preset="veryslow")
-)
+def main():
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        print("Please enter a file name.")
+        return
 
-ffmpeg.execute()
+    output_name: str = clean_name(file_name)
+
+    ffmpeg = (
+        FFmpeg()
+        .input(input_name)
+        .output(output_name, {"codec:a": "libmp3lame", "q:a": "0"}, preset="veryslow")
+    )
+
+    ffmpeg.execute()
+
+
+if __name__ == "__main__":
+    main()
