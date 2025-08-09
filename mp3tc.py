5075f50cb9264aabf6943e98cb81e08a5051685b
Aaron <aaron.stacey@proton.me>
2025-08-09 09:49:39 -0500

    Move ffmpeg to it's own function `transcode`

--- mp3tc.py
+++ mp3tc.py
@@ -1,11 +1,29 @@
 import sys
-from ffmpeg import FFmpeg
+from ffmpeg import FFmpeg, errors
 
 
 def clean_name(input_name: str) -> str:
     output_name = input_name.split(".")[0]
     output_name += ".mp3"
     return output_name
+
+
+def transcode(input_name: str, output_name: str):
+    try:
+        ffmpeg = (
+            FFmpeg()
+            .input(input_name)
+            .output(
+                output_name, {"codec:a": "libmp3zlame", "q:a": "0"}, preset="veryslow"
+            )
+        )
+
+        ffmpeg.execute()
+    except errors.FFmpegUnsupportedCodec:
+        print("Invalid encoder, is lame installed?")
+        return
+        
+    return
 
 
 def main():
@@ -17,13 +35,7 @@ def main():
 
     output_name: str = clean_name(file_name)
 
-    ffmpeg = (
-        FFmpeg()
-        .input(input_name)
-        .output(output_name, {"codec:a": "libmp3lame", "q:a": "0"}, preset="veryslow")
-    )
-
-    ffmpeg.execute()
+    transcode(file_name, output_name)
 
 
 if __name__ == "__main__":
